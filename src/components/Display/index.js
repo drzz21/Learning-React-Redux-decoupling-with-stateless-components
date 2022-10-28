1da6293ad2a15c1836ce6d77955586ecf9cb55aa
Francisco D <drz.y2k@gmail.com>
2022-10-28 17:33:34 -0500

    8. Making Display stateless

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -1,33 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ee from '../../eventEmitter'
 
-let state = '0';
-
-
-
-const updateDisplay = (newStr) => {
-  // console.log(this.state);
-  state = ({ text: newStr.toString().split(' ').reverse().join(' ') });
-}
-
-// componentWillMount() {
-
-// }
-
-// onClickHandler() {
-//   if (this.props.clickHandler) {
-//     this.props.clickHandler.call(this);
-//   }
-// }
-
-
-
-
 export const Display = ({ text, clickHandler }) => {
-  ee.addListener('displayUpdate', updateDisplay);
-  state = { text: text || '0' };
-
-  return <div className="display" onClick={clickHandler ? clickHandler : null}>{state.text}</div>
+  // ee.addListener('displayUpdate', updateDisplay);
+  return <div className="display" onClick={clickHandler ? clickHandler : null}>{text.toString().split(' ').reverse().join(' ')}</div>
 }
 
 
